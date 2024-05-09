d7446a7663352991d6e8cdb71c83e8ada50da97c
Elton Santos <elton.c.s1979@gmail.com>
2024-05-09 17:19:10 -0300

    Aprendendo sobre concatenação

---
+++ types_variables/src/Operators.java
@@ -0,0 +1,30 @@
+public class Operators {
+    public static void main(String[] args) {
+        //classe Operadores.java
+            String fullName = "LINGUAGEM" + "JAVA";
+            System.out.println(fullName);
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}

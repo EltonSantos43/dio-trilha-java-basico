07901ac348fe3737a3f21f4ed7acfaadd140b559
Elton Santos <elton.c.s1979@gmail.com>
2024-05-09 16:24:15 -0300

    Estudo sobre tipos e variáveis

--- types_variables/src/VariableTypes.java
+++ types_variables/src/VariableTypes.java
@@ -1,5 +1,22 @@
 public class VariableTypes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Primitive Types
+        //Study the String class that represents text in the application
+
+        String myName = "Elton Santos";
+
+        double MinimumWage = 2500;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short)normalNumber;
+
+        int number = 5;
+
+        number = 10;
+
+        System.out.print(number);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }

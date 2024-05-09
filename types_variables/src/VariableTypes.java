4bfc7354e536ee3ec9479f7a5514e082b7d93aae
Elton Santos <elton.c.s1979@gmail.com>
2024-05-09 16:31:40 -0300

    Usando as variáveis

--- types_variables/src/VariableTypes.java
+++ types_variables/src/VariableTypes.java
@@ -4,19 +4,23 @@ public class VariableTypes {
         // Study the String class that represents text in the application
 
         String myName = "Elton Santos";
+        System.out.println(myName);
 
         double MinimumWage = 2500;
+        System.out.println(MinimumWage);
 
         short shortNumber = 1;
         int normalNumber = shortNumber;
         short shortNumber2 = (short) normalNumber;
+        System.out.println(shortNumber2);
 
         int number = 5;
 
         number = 10;
 
-        System.out.print(number);
+        System.out.println(number);
 
         final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
